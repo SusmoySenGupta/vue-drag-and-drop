5dd2612480ffb3470badde617e01a2276a891c2a
Susmoy <susmoy.cse@gmail.com>
2021-05-13 21:28:08 +0600

    add initial design & setup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<div class="drop-zone">
+		<div v-for="item in getList(1)" :key="item.id" class="drag-el">
+			{{ item.title }}
+		</div>
+	</div>
+	<div class="drop-zone">
+		<div v-for="item in getList(2)" :key="item.id" class="drag-el">
+			{{ item.title }}
+		</div>
+	</div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { ref } from "vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+	setup() {
+		const items = ref([
+			{ id: 0, title: "Item A", list: 1 },
+			{ id: 1, title: "Item B", list: 1 },
+			{ id: 2, title: "Item C", list: 2 },
+		]);
+
+		const getList = (list) => {
+			return items.value.filter((item) => item.list == list);
+		};
+
+		return { getList };
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.drop-zone {
+	width: 50%;
+	margin: 50px auto;
+	background-color: #ecf0f1;
+	padding: 10px;
+	min-height: 10px;
+}
+
+.drag-el {
+    background-color: #3498db;
+    color: #fff;
+    padding: 5px;
+    margin-bottom: 10px;
+}
+
+.drag-el:nth-last-of-type(1) {
+    margin-bottom: 0;
 }
 </style>
